24f8318b6554ae93b13c375c9deae8cd34ceb9c6
lonespartan12 <56203064+lonespartan12@users.noreply.github.com>
2020-02-10 19:20:04 -0700

    use NaN to verify a number

--- JS/Basic_JavaScript_5.js
+++ JS/Basic_JavaScript_5.js
@@ -2,5 +2,6 @@ document.write(typeof"keystone");
 
 function myFunction() {
     //document.getElementById("Test").innerHTML=0/0;
-    document.getElementById("Test").innerHTML=isNaN('This is a string');
+    //document.getElementById("Test").innerHTML=isNaN('This is a string');    
+    document.getElementById("Test").innerHTML=isNaN('007');
 }
