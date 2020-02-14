48efa647d58655c3164bef92ecbb19f06b889cea
lonespartan12 <56203064+lonespartan12@users.noreply.github.com>
2020-02-14 15:20:57 -0700

    if statement toggle

--- JS/Basic_JavaScript_7.js
+++ JS/Basic_JavaScript_7.js
@@ -10,12 +10,13 @@ function addNumbers2() {
 
 function addNumbers3() {
     var y = 10;
-    //document.write(20+y+"<br>");
+    document.write(20+y+"<br>");
     console.log(20+y+"<br>");
 }
 
 function addNumbers4() {
-    //document.write(y+100+"<br>"+"<br>");
+    y=20;
+    document.write(y+100+"<br>"+"<br>");
     console.log(y+100+"<br>"+"<br>");
 }
 
@@ -28,4 +29,18 @@ function getDate() {
     if (new Date().getHours() <18) {
         document.getElementById("greeting").innerHTML="Good Afternoon!";
     }
+}
+
+var g=false;
+function btnColor(){    
+    if (g==true) {
+        color="green";
+        g=false;
+        document.getElementById("Btn").style.background=color;
+    }else{
+        color="red";
+        g=true;
+        document.getElementById("Btn").style.background=color;
+    }
+    
 }
