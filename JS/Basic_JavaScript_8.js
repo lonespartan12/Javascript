ece79d1b60fdd5717d208f5df8f16d6d916dd550
lonespartan12 <56203064+lonespartan12@users.noreply.github.com>
2020-02-21 14:47:20 -0700

    slice method
    also added power mode

--- JS/Basic_JavaScript_8.js
+++ JS/Basic_JavaScript_8.js
@@ -6,3 +6,9 @@ function full_sentence() {
     var whole_sentence=part1.concat(part2,part3,part4);
     document.getElementById("concat").innerHTML=whole_sentence;
 }
+
+function slice_Method() {
+    var sentence = "All work and no play makes Johnny a dull boy.";
+    var section = sentence.slice(27,33);
+    document.getElementById("slice").innerHTML=section;
+}
