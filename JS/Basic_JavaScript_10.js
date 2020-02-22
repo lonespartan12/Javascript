bdfcd16b21aa235597c6774568fee2b78bfd12a3
lonespartan12 <56203064+lonespartan12@users.noreply.github.com>
2020-02-22 14:25:04 -0700

    instrument list

--- JS/Basic_JavaScript_10.js
+++ JS/Basic_JavaScript_10.js
@@ -1,3 +1,14 @@
 var str="Hello World!";
 var n=str.length;
-document.write(n);
+document.write(n+"<br>");
+
+var instruments=["guitar", "drums", "piano", "bass", "violin", "trumpet", "flute"];
+var content="";
+var y;
+
+function for_loop(){
+    for(y=0; y<instruments.length; y++) {
+        content += instruments[y] + "<br>";
+    }
+    document.getElementById("list_of_instruments").innerHTML=content;
+}
