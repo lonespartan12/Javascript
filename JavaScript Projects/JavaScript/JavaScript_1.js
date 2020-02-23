14cb6173ce428a79b61cdca3658f5aa038290426
lonespartan12 <56203064+lonespartan12@users.noreply.github.com>
2020-02-22 17:50:04 -0700

    power ranger switch

---
+++ JavaScript Projects/JavaScript/JavaScript_1.js
@@ -0,0 +1,28 @@
+function ranger() {
+    var favorite_ranger;
+    var rangers=document.getElementById("color_input").value;
+    var ranger_string= " Ranger is your favorite Power Ranger"
+    switch(rangers) {
+        case "Red":
+            favorite_ranger="Red" + ranger_string;
+        break;
+        case "Yellow":
+            favorite_ranger="Yellow" + ranger_string;
+        break;
+        case "Green":
+            favorite_ranger="Green" + ranger_string;
+        break;
+        case "Blue":
+            favorite_ranger="Blue" + ranger_string;
+        break;
+        case "Pink":
+            favorite_ranger="Pink" + ranger_string;
+        break;
+        case "Black":
+            favorite_ranger="Black" + ranger_string;
+        break;
+        default:
+            favorite_ranger="Zordon is better than any Power Ranger";
+    }
+    document.getElementById("output").innerHTML=favorite_ranger;
+}
