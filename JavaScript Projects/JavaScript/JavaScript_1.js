9a26bf05ceab21b77eeac6b1770c7944052f3592
lonespartan12 <56203064+lonespartan12@users.noreply.github.com>
2020-02-22 21:29:15 -0700

    gradient

--- JavaScript Projects/JavaScript/JavaScript_1.js
+++ JavaScript Projects/JavaScript/JavaScript_1.js
@@ -31,3 +31,13 @@ function Hello_World_Function() {
     var A = document.getElementsByClassName("click");
     A[0].innerHTML="The text has changed!";
 }
+
+function gradient() {
+    var c= document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    var grd = ctx.createLinearGradient(0,0,170,0);
+    grd.addColorStop(0,"black");
+    grd.addColorStop(1,"white");
+    ctx.fillStyle=grd;
+    ctx.fillRect(20,20,150,100);
+}
